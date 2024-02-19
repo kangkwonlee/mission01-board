dc16e3dfe25864e4134fa59f8b858e680410c208
Ohgiraffers-bear <bear.ohgiraffers@gmail.com>
2024-02-19 13:00:05 +0900

    [Feat] : 게시글 작성 및 단건 조회기능 구현

--- src/main/java/org/ohgiraffers/board/controller/PostController.java
+++ src/main/java/org/ohgiraffers/board/controller/PostController.java
@@ -1,9 +1,61 @@
 package org.ohgiraffers.board.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.ohgiraffers.board.domain.dto.CreatePostRequest;
+import org.ohgiraffers.board.domain.dto.CreatePostResponse;
+import org.ohgiraffers.board.domain.dto.ReadPostResponse;
+import org.ohgiraffers.board.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
 /** 레이어드 아키텍쳐
  * 소프트웨어를 여러개의 계층으로 분리해서 설계하는 방법
  * 각 계층이 독립적으로 구성되서, 한 계층이 변경이 일어나도, 다른 계층에 영향을 주지 않는다.
  * 따라서 코드의 재사용성과 유지보수성을 높일 수 있다.
  */
+
+/** Controller RestController
+ * Controller 주로 화면 View 를 반환 하기 위해 사용된다.
+ * 하지만 종종 Controller를 쓰면서도 데이터를 반환 해야 할 때가 있는데, 이럴 대 사용하는 것이 @ResponseBody 이다.
+ *
+ * REST란?
+ * Representational AState Transfer의 약자
+ * 자원을 이름으로 구분하여 자원의 상태를 주고 받는 것을 의미한다.
+ * REST는 기본적으로 웹의 기존 기술과 HTTP 프로토콜을 그대로 사용하기 때문에,
+ * 웹의 장점을 최대한 활용 할 수 있는 아키텍쳐 스타일이다.
+ */
+
+@Controller
+@ResponseBody
+// @RequestMapping : 특정 URL을 매핑하게 도와준다.
+@RequestMapping("/api/v1/posts")
+// @RequiredArgsConstructor : final 혹은 @NonNull 어노테이션이 붙은 필드에 대한 생성자를 자동으로 생성해준다.
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<CreatePostResponse> postCreate(@RequestBody CreatePostRequest request) {
+
+        CreatePostResponse response = postService.createPost(request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<ReadPostResponse> postRead(@PathVariable Long postId) {
+
+        ReadPostResponse response = postService.readPostById(postId);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
+
+
+
+
+
+
--- src/main/java/org/ohgiraffers/board/service/PostService.java
+++ src/main/java/org/ohgiraffers/board/service/PostService.java
@@ -1,4 +1,62 @@
 package org.ohgiraffers.board.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.ohgiraffers.board.domain.dto.CreatePostRequest;
+import org.ohgiraffers.board.domain.dto.CreatePostResponse;
+import org.ohgiraffers.board.domain.dto.ReadPostResponse;
+import org.ohgiraffers.board.domain.entity.Post;
+import org.ohgiraffers.board.repository.PostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/** Service 를 인터페이스와 구현체로 나누는 이유
+ * 1.다형성과 OCP원칙을 지키기 위해
+ * 인터페이스와 구현체가 나누어지면, 구현체는 외부로부터 독립되어, 구현체의 수정이나 확장이 자유로워진다.
+ * 2.관습적인 추상화 방식
+ * 과거, Spring에서 AOP를 구현 할대 JDK Dynamic Proxy 를 사용했는데, 이때 인터페이스가 필수였다.
+ * 지금은, CGLB를 기본적으로 포함하여 클래스 기반을 프록시 객체를 생성 할 수 있게 되었다.
+ */
+/** @Transactional
+ * 선언적으로 트랜젝션 관리를 가능하게 해준다.
+ * 메서드가 실행되는 동안 모든 데이터베이스 연산을 하나의 트랜잭션으로 묶어 처리한다.
+ * 이를통해, 메서드 내에서 데이터베이스 상태를 변경하는 작업들이 모두 성공적으로 완료되면 그 변경사항을 commit 하고
+ * 하나라도 실패하면 모든 변경사항을 rollback 시켜 관리한다.
+ *
+ * Transaction
+ * 데이터베이스의 상태를 변화시키기 위해 수행하는 작업의 단위
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public CreatePostResponse createPost(CreatePostRequest request) {
+
+        Post post = Post.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+
+        Post savedPost = postRepository.save(post);
+
+        return new CreatePostResponse(savedPost.getPostId(), savedPost.getTitle(), savedPost.getContent());
+    }
+
+    public ReadPostResponse readPostById(Long postId) {
+
+        Post foundPost = postRepository.findById(postId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 postId로 조회된 게시글이 없습니다."));
+
+        return new ReadPostResponse(foundPost.getPostId(), foundPost.getTitle(), foundPost.getContent());
+    }
 }
+
+
+
+
+
+
+
